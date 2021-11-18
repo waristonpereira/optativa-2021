a8975164d307b5410daa744a81096b8e5cf591c4
Wariston Pereira <waristonfernando@gmail.com>
2021-11-18 20:55:38 -0300

    Melhorias no modelo de usuários

--- sistema/modelos/usuario.py
+++ sistema/modelos/usuario.py
@@ -3,18 +3,51 @@ from base.modelo import Modelo
 class Usuario(Modelo):
     Tabela = "usuarios"
 
-    def __init__(self):
+    def __init__(self, id=None):
         super().__init__(self.Tabela);
         # Atributos da tabela 'usuarios'
-        self.id = None;
-        self.usuario = None;
-        self.senha = None;
+        self.id = id;
+        if id:
+            dado = self.obter_por_id(id)
+            if dado:
+                self.usuario = dado[1];
+                self.senha = dado[2];
+            else:
+                self.id = None;
+                self.usuario = None;
+                self.senha = None;
+        else:
+            self.usuario = None;
+            self.senha = None;
     
     def salvar(self):
         try:
-            instrucao = "INSERT INTO {tabela} (usuario, senha) VALUES (%s, %s)".format(tabela = self.Tabela)
-            self.obter_cursor().execute(instrucao, [self.usuario, self.senha])
+            if self.id:
+                instrucao = "UPDATE {tabela} SET usuario=%s, senha=%s WHERE id=%s".format(tabela = self.Tabela)
+                self.obter_cursor().execute(instrucao, [self.usuario, self.senha, self.id])
+            else:
+                instrucao = "INSERT INTO {tabela} (usuario, senha) VALUES (%s, %s)".format(tabela = self.Tabela)
+                self.obter_cursor().execute(instrucao, [self.usuario, self.senha])
+                self.id = self.obter_cursor().lastrowid
             return True
         except:
             print ("Ocorreu um erro ao executar: ", self.obter_cursor()._last_executed)
             return False
+    
+    def excluir(self):
+        if self.id:
+            self.excluir_por_id(self.id)
+            return True
+        else:
+            print(f"Não é possível excluir {self.id}")
+            return False
+
+    def obter_por_nome(self, nome):
+        try:
+            instrucao = "SELECT * FROM {tabela} WHERE usuario LIKE %s".format(tabela = self.Tabela)
+            self.obter_cursor().execute(instrucao, nome)
+            resultado = self.obter_cursor().fetchone()
+            return resultado
+        except:
+            print ("Ocorreu um erro ao executar: ", self.obter_cursor()._last_executed)
+            return None
