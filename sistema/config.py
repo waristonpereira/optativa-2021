f5e39cc0b1d332abea1fea25c57034f8c8982903
Wariston Pereira <waristonfernando@gmail.com>
2021-11-25 21:35:55 -0300

    Limpar config

--- sistema/config.py
+++ sistema/config.py
@@ -1,4 +1,4 @@
-MYSQL_HOST    = 'db4free.net'
-MYSQL_USUARIO = 'yibivo2308'
-MYSQL_SENHA   = 'yibivo2308'
-MYSQL_BANCO   = 'yibivo2308'
+MYSQL_HOST    = 'endereco do servidor'
+MYSQL_USUARIO = 'usuario'
+MYSQL_SENHA   = 'senha'
+MYSQL_BANCO   = 'nome do banco'
